0bd60ed34fb54de2a4105ee9ee3b3518338c7b8a
leelakrishna24 <56736849+leelakrishna24@users.noreply.github.com>
2023-03-27 16:40:01 +0530

    Exercise

---
+++ quiz1.py
@@ -0,0 +1,26 @@
+# list = [0,1,2,3,4]
+# print(list[::2])
+
+# def solve(a):
+#     return a[-1]
+# a = {1,2,3,4}
+# print(solve(a)**2)
+
+# def fun1(num):
+#     return num + 25
+#
+# ans = fun1(5)
+# print(ans)
+
+if __name__ == '__main__':
+    n = int(input("n:"))
+
+i = 0
+while i in range(0, n):
+    num = input()
+    
+    i = i + 1
+
+while i in range(0,n):
+    print(n(i))
+    i = i + 1
