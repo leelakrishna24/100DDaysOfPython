3d8dba8714fcf35e59f25403c0dea524c0ca5701
leelakrishna24 <56736849+leelakrishna24@users.noreply.github.com>
2023-04-20 08:59:04 +0530

    Exercises

--- 19_4_2023/Celsius_Farenheit_viceversa_conversion.py
+++ 19_4_2023/Celsius_Farenheit_viceversa_conversion.py
@@ -1,21 +1,21 @@
 # Write a Python program to convert temperatures from Celsius to Fahrenheit and vice versa
-
+#take the temperature measure input
 Temperature = input("Enter the temperature including the scale name : ")
-
 print(Temperature)
 
+#Replace the reading scale name with empty
 temperature = Temperature.replace("C" or "F","")
 print(temperature)
 
-
+# Convert the value to
 def Conversion(n):
     if "C" in Temperature:
         print("It is Celsius value")
-        return (((int(n)*9)/5)+32)
+        return str(((int(n)*9)/5)+32)+ "F"
 
     elif "F" in Temperature:
         print("it is Farenheit Value")
-        return (((int(n)-32)*5)/9)
+        return str(((int(n)-32)*5)/9)+ "C"
 
 Conversion(temperature)
 print(Conversion(temperature))
