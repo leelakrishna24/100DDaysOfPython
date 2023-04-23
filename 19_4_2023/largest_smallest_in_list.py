7ef0ec74b33a45f01d2025747c1234532419de90
leelakrishna24 <56736849+leelakrishna24@users.noreply.github.com>
2023-04-23 22:33:53 +0530

    Exercises

--- 19_4_2023/largest_smallest_in_list.py
+++ 19_4_2023/largest_smallest_in_list.py
@@ -1,15 +1,14 @@
-#program for finding largest and smallest number in a list
-
+# program for finding largest and smallest number in a list
 my_list = [8,2,4,12,6,0,10]
 
-print(min(my_list))
-print(max(my_list))
+# print(min(my_list))
+# print(max(my_list))
 
-#initialise variables for small and large
+# initialise variables for small and large
 largest_number = my_list[0]
 smallest_number = my_list[0]
 
-#loop through list of numbers to find smallest and largest number
+# loop through list of numbers to find smallest and largest number
 for num in my_list:
     if num > largest_number:
         largest_number = num
--- 20_4_2023/Fibonnaci_sequence.py
+++ 20_4_2023/Fibonnaci_sequence.py
@@ -2,7 +2,7 @@
 
 num = int(input("Enter the number till which you want to find the fibonacci sequence"))
 
-
+# Define a function as fibonacci and loop
 def fibonacci(num):
     if num <= -1:
         print("Enter positive number")
@@ -10,5 +10,6 @@ def fibonacci(num):
         return 0
     elif num == 1:
         return 1
+    else:
+        return fibonacci(num-1) + fibonacci(num-2)
 
-# yet to be completed
