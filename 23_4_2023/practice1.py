593bbcf6ea63093969c6fa5963e6bfb86939cdf6
leelakrishna24 <56736849+leelakrishna24@users.noreply.github.com>
2023-04-25 14:46:51 +0530

    Exercises

---
+++ 23_4_2023/practice1.py
@@ -0,0 +1,8 @@
+amount_of_apples = 5
+price_of_apples = 25
+entire_cost_of_apples = amount_of_apples * price_of_apples
+print(entire_cost_of_apples)
+
+inputstring = input("Enter a string ")
+print(inputstring,sep='#',end='&')
+
